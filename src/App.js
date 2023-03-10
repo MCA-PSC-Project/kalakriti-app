978b1720437d2f5556dfef392f3b0ba0e581ceb0
Prashant Kumar <prashantbittu123@gmail.com>
2023-03-10 12:55:08 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
