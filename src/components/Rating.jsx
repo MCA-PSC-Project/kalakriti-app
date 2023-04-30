1112fa578256f3441f28e4594bbb2037993d4c21
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-04-30 21:25:08 +0530

    fontawesome

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,26 +1,12 @@
 import React, { useState } from "react";
-import './Rating.css';
+import "./Rating.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function Rating() {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
   return (
-    <div className="star-rating">
-      {[...Array(5)].map((star, index) => {
-        index += 1;
-        return (
-          <button
-            type="button"
-            key={index}
-            className={index <= (hover || rating) ? "on" : "off"}
-            onClick={() => setRating(index)}
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(rating)}
-          >
-            <span className="star">&#9733;</span>
-          </button>
-        );
-      })}
+    <div className="ratings">
+      <FontAwesomeIcon icon={faStar} />
     </div>
   );
 }
