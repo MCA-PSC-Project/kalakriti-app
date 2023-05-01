b5fced1d54af18e6daa0a2a8c8bca90ba7994eb9
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-05-01 16:05:45 +0530

    rating star fixed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,12 @@
 function Carousel({ imgSrcList }) {
   return (
-    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
+    <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-indicators">
         <button
           type="button"
           data-bs-target="#myCarousel"
           data-bs-slide-to="0"
-          class=""
+          className=""
           aria-label="Slide 1"
         ></button>
         <button
@@ -14,7 +14,7 @@ function Carousel({ imgSrcList }) {
           data-bs-target="#myCarousel"
           data-bs-slide-to="1"
           aria-label="Slide 2"
-          class="active"
+          className="active"
           aria-current="true"
         ></button>
         <button
@@ -22,70 +22,70 @@ function Carousel({ imgSrcList }) {
           data-bs-target="#myCarousel"
           data-bs-slide-to="2"
           aria-label="Slide 3"
-          class=""
+          className=""
         ></button>
       </div>
-      <div class="carousel-inner">
-        <div class="carousel-item">
+      <div className="carousel-inner">
+        <div className="carousel-item">
           <img
             src={imgSrcList[0]}
             className="d-block w-100"
             alt="..."
             style={{ width: "100%", height: "400px" }}
           />
-          <div class="container">
-            <div class="carousel-caption text-start">
+          <div className="container">
+            <div className="carousel-caption text-start">
               <h1>Example headline.</h1>
               <p>
                 Some representative placeholder content for the first slide of
                 the carousel.
               </p>
               <p>
-                <a class="btn btn-lg btn-primary" href="#">
+                <a className="btn btn-lg btn-primary" href="#">
                   Sign up today
                 </a>
               </p>
             </div>
           </div>
         </div>
-        <div class="carousel-item active">
+        <div className="carousel-item active">
           <img
             src={imgSrcList[1]}
             className="d-block w-100"
             alt="..."
             style={{ width: "100%", height: "400px" }}
           />
-          <div class="container">
-            <div class="carousel-caption">
+          <div className="container">
+            <div className="carousel-caption">
               <h1>Another example headline.</h1>
               <p>
                 Some representative placeholder content for the second slide of
                 the carousel.
               </p>
               <p>
-                <a class="btn btn-lg btn-primary" href="#">
+                <a className="btn btn-lg btn-primary" href="#">
                   Learn more
                 </a>
               </p>
             </div>
           </div>
         </div>
-        <div class="carousel-item">
+        <div className="carousel-item">
           <img
             src={imgSrcList[2]}
             className="d-block w-100"
             alt="..."
             style={{ width: "100%", height: "400px" }}
           />
-          <div class="container">
-            <div class="carousel-caption text-end">
+          <div className="container">
+            <div className="carousel-caption text-end">
               <h1>One more for good measure.</h1>
               <p>
                 Some representative placeholder content for the third slide of
                 this carousel.
               </p>
               <p>
-                <a class="btn btn-lg btn-primary" href="#">
+                <a className="btn btn-lg btn-primary" href="#">
                   Browse gallery
                 </a>
               </p>
@@ -94,22 +94,22 @@ function Carousel({ imgSrcList }) {
         </div>
       </div>
       <button
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         type="button"
         data-bs-target="#myCarousel"
         data-bs-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
       </button>
       <button
-        class="carousel-control-next"
+        className="carousel-control-next"
         type="button"
         data-bs-target="#myCarousel"
         data-bs-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
       </button>
     </div>
   );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faStar,
-  faStarHalfStroke,
-} from "@fortawesome/free-solid-svg-icons";
+import { faStar, faStarHalfStroke } from "@fortawesome/free-regular-svg-icons";
+import { faStar as faStarFilled } from "@fortawesome/free-solid-svg-icons";
 
 function Rating({ ratingValue }) {
   const rating = parseFloat(ratingValue);
-  const ratingInteger = Math.floor(rating);
+  let ratingInteger = Math.floor(rating);
   const ratingFractional = rating - ratingInteger;
   console.log(ratingValue, rating, ratingInteger, ratingFractional);
   const elements = [];
   for (let i = 1; i <= ratingInteger; i++) {
     elements.push(
-      <FontAwesomeIcon icon={faStar} size="xl" style={{ color: "#ffff00" }} />
+      <FontAwesomeIcon
+        icon={faStarFilled}
+        size="xl"
+        style={{ color: "#ffff00" }}
+      />
     );
   }
-  if (ratingFractional) {
+  if (ratingFractional > 0 && ratingFractional <= 0.5) {
     elements.push(
       <FontAwesomeIcon
         icon={faStarHalfStroke}
@@ -24,6 +26,20 @@ function Rating({ ratingValue }) {
         style={{ color: "#ffff00" }}
       />
     );
+    ratingInteger += 1;
+  } else if (ratingFractional > 0.5) {
+    elements.push(
+      <FontAwesomeIcon
+        icon={faStarFilled}
+        size="xl"
+        style={{ color: "#ffff00" }}
+      />
+    );
+    ratingInteger += 1;
+  }
+  // remaining stars
+  for (let i = ratingInteger + 1; i <= 5; i++) {
+    elements.push(<FontAwesomeIcon icon={faStar} size="xl" />);
   }
   return <div className="ratings">{elements}</div>;
 }
