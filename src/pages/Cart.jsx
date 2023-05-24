aa2e382b68f196f6328d233fd7964d986a22f1e1
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-05-24 17:29:53 +0530

    Cart horizontal card

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,107 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Logo from "../assets/logo.jpeg";
+import Rating from "../components/Rating";
+import { Link } from "react-router-dom";
 
 function Cart() {
   return (
     <>
       <NavBar />
       <div>Cart</div>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="text-left">
+          <CartHorizontalCard
+            imgSrc={Logo}
+            cardTitle="product"
+            sellerName="seller_name"
+            originalPrice="1000"
+            offerPrice="500"
+            minOrderQuantity={1}
+            maxOrderQuantity={5}
+          />
+          <CartHorizontalCard
+            imgSrc={Logo}
+            cardTitle="product"
+            sellerName="seller_name"
+            originalPrice="1000"
+            offerPrice="500"
+            minOrderQuantity={5}
+            maxOrderQuantity={10}
+          />
+          <CartHorizontalCard
+            imgSrc={Logo}
+            cardTitle="product"
+            sellerName="seller_name"
+            originalPrice="1000"
+            offerPrice="500"
+            minOrderQuantity={2}
+            maxOrderQuantity={6}
+          />
+        </div>
+      </div>
       <Footer />
     </>
+  );
+}
+
+function CartHorizontalCard({
+  imgSrc,
+  cardTitle,
+  sellerName,
+  originalPrice,
+  offerPrice,
+  minOrderQuantity,
+  maxOrderQuantity,
+}) {
+  const elements = [];
+  for (let i = minOrderQuantity; i <= maxOrderQuantity; i++) {
+    elements.push(
+      <li>
+        <a className="dropdown-item" href="#">
+          {i}
+        </a>
+      </li>
+    );
+  }
+
+  return (
+    <div className="card mb-3" style={{ maxWidth: 850 }}>
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={imgSrc} className="img-fluid rounded-start" alt="..." />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h2 className="card-title">{cardTitle}</h2>
+            <h6>by {sellerName}</h6>
+            <p className="card-text">
+              <span>&#8377;</span>
+              <del>{originalPrice}</del>&nbsp;
+              <span>&#8377;</span>
+              {offerPrice}
+            </p>
+            <div className="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
+              <div className="dropdown">
+                <button
+                  type="button"
+                  className="btn btn-primary dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                >
+                  Quantity: {minOrderQuantity}
+                </button>
+                <ul className="dropdown-menu">{elements}</ul>
+              </div>
+
+              <button type="button" class="btn btn-outline-danger">
+                Remove From Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import HorizontalCard from "../components/HorizontalCard";
 import Logo from "../assets/logo.jpeg";
+import Rating from "../components/Rating";
+import { Link } from "react-router-dom";
 
 function Wishlist() {
   return (
@@ -11,7 +12,7 @@ function Wishlist() {
       <h1>Wishlist</h1>
       <div className="d-flex justify-content-center align-items-center">
         <div className="text-left">
-          <HorizontalCard
+          <WishlistHorizontalCard
             imgSrc={Logo}
             cardTitle="product"
             sellerName="seller_name"
@@ -20,7 +21,7 @@ function Wishlist() {
             average_rating={4.5}
             ratingCount={5}
           />
-          <HorizontalCard
+          <WishlistHorizontalCard
             imgSrc={Logo}
             cardTitle="product"
             sellerName="seller_name"
@@ -29,7 +30,7 @@ function Wishlist() {
             average_rating={4.5}
             ratingCount={5}
           />
-          <HorizontalCard
+          <WishlistHorizontalCard
             imgSrc={Logo}
             cardTitle="product"
             sellerName="seller_name"
@@ -42,6 +43,61 @@ function Wishlist() {
       </div>
       <Footer />
     </>
+  );
+}
+
+function WishlistHorizontalCard({
+  imgSrc,
+  cardTitle,
+  sellerName,
+  originalPrice,
+  offerPrice,
+  average_rating,
+  ratingCount,
+}) {
+  return (
+    <div className="card mb-3" style={{ maxWidth: 850 }}>
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={imgSrc} className="img-fluid rounded-start" alt="..." />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h2 className="card-title">{cardTitle}</h2>
+            <h6>by {sellerName}</h6>
+            <p className="card-text">
+              <Rating ratingValue={average_rating} ratingCount={ratingCount} />
+              <p className="card-text">
+                <span>&#8377;</span>
+                <del>{originalPrice}</del>&nbsp;
+                <span>&#8377;</span>
+                {offerPrice}
+              </p>
+            </p>
+            <div className="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
+              <Link
+                to=""
+                className="btn border px-2 shadow-0 me-1"
+                title="Add to cart"
+              >
+                <button type="button" class="btn btn-outline-primary">
+                  Add To Cart
+                </button>
+              </Link>
+              <Link
+                to=""
+                className="btn border px-2 shadow-0 me-1"
+                title="Remove From Wishlist"
+              >
+                <button type="button" class="btn btn-outline-danger">
+                  Remove From Wishlist
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
