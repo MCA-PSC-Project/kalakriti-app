7d78f9d448f2886c09a0c767a5d0cc70b4f2c8dc
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-06-16 14:51:53 +0530

    rating mouseover and on click animate

--- src/components/GiveRating.jsx
+++ src/components/GiveRating.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { faStar as faStarFilled } from "@fortawesome/free-solid-svg-icons";
 
+function StarIcon({ index, icon, color, setRating, setHoverRating }) {
+  const handleMouseEnter = () => {
+    setHoverRating(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverRating(0);
+  };
+
+  const handleClick = () => {
+    setRating(index);
+  };
+
+  return (
+    <>
+      <FontAwesomeIcon
+        icon={icon}
+        size="xl"
+        style={{ color: color }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
+      />
+    </>
+  );
+}
+
 function GiveRating() {
-  const elements = [];
-  for (let i = 1; i <= 5; i++) {
-    elements.push(
-      <FontAwesomeIcon icon={faStar} size="xl" style={{ color: "#000000" }} />
-    );
-  }
+  const [rating, setRating] = useState();
+  const [hoverRating, setHoverRating] = useState(0);
+
   return (
     <div>
       <h1>Rating: </h1>
-      {elements}
+      {[1, 2, 3, 4, 5].map((index) => (
+        <StarIcon
+          key={index}
+          index={index}
+          icon={index <= (hoverRating || rating) ? faStarFilled : faStar}
+          color={index <= (hoverRating || rating) ? "#ffff00" : "#000000"}
+          setRating={setRating}
+          setHoverRating={setHoverRating}
+        />
+      ))}
     </div>
   );
 }
