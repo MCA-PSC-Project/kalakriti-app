5b945641cf8f210da5609689419eb3cdded80e78
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-06-18 15:32:41 +0530

    Product page navigate fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import WishlistSvg from "../assets/Heart.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Rating from "./Rating";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import api from "../utils/api";
@@ -17,6 +17,7 @@ function Card({
   ratingCount,
   minOrderQuantity,
 }) {
+  const navigate = useNavigate();
   const [showToast, setShowToast] = useState(false);
   const [toastProperties, setToastProperties] = useState({});
 
@@ -40,77 +41,84 @@ function Card({
           onClose={() => setShowToast(false)}
         />
       )}
-      <div className="col-lg-3 col-md-6 col-sm-6 d-flex">
-        <Link
-          to={`/products/${productId}`}
-          style={{ color: "inherit", textDecoration: "none" }}
-        >
-          <div className="card w-100 my-2 shadow-2-strong">
-            <img
-              src={imgSrc}
-              className="card-img-top"
-              style={{ aspectRatio: "1 / 1" }}
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">{cardTitle}</h5>
-              <Rating ratingValue={average_rating} ratingCount={ratingCount} />
-              <p className="card-text">
-                <span>
-                  <del>&#8377;{originalPrice}</del>
-                </span>
-                &nbsp;
-                <span>
-                  <b>&#8377;{offerPrice}</b>
-                </span>
-              </p>
-              <div className="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                {/* Add to cart */}
-                <button
-                  type="button"
-                  className="btn btn-warning me-2"
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    api
-                      .post("/carts", {
-                        product_item_id: productItemId,
-                        quantity: minOrderQuantity,
-                      })
-                      .then((response) => {
-                        if (response.data) {
-                          console.log("item added to cart successfully");
-                          setShowToast(true);
-                          setToastProperties({
-                            toastType: "success",
-                            toastMessage: "Item added to Cart successfully",
-                          });
-                        }
-                      })
-                      .catch((error) => {
-                        console.error("some error occured in adding to cart");
-                        console.error(error);
+      <div
+        className="col-lg-3 col-md-6 col-sm-6 d-flex"
+        onClick={(event) => {
+          // Check if clicked element is button like "Add to cart"
+          if (event.target.closest(".btn")) {
+            // Do not navigate if clicked element is button like "Add to cart"
+            // console.log("button click event");
+            return;
+          }
+
+          // Navigate to product page
+          navigate(`/products/${productId}`);
+        }}
+      >
+        <div className="card w-100 my-2 shadow-2-strong">
+          <img
+            src={imgSrc}
+            className="card-img-top"
+            style={{ aspectRatio: "1 / 1" }}
+          />
+          <div className="card-body d-flex flex-column">
+            <h5 className="card-title">{cardTitle}</h5>
+            <Rating ratingValue={average_rating} ratingCount={ratingCount} />
+            <p className="card-text">
+              <span>
+                <del>&#8377;{originalPrice}</del>
+              </span>
+              &nbsp;
+              <span>
+                <b>&#8377;{offerPrice}</b>
+              </span>
+            </p>
+            <div className="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
+              {/* Add to cart */}
+              <button
+                type="button"
+                className="btn btn-warning me-2"
+                onClick={() => {
+                  api
+                    .post("/carts", {
+                      product_item_id: productItemId,
+                      quantity: minOrderQuantity,
+                    })
+                    .then((response) => {
+                      if (response.data) {
+                        console.log("item added to cart successfully");
                         setShowToast(true);
                         setToastProperties({
-                          toastType: "error",
-                          toastMessage: "some error occured in adding to cart",
+                          toastType: "success",
+                          toastMessage: "Item added to Cart successfully",
                         });
+                      }
+                    })
+                    .catch((error) => {
+                      console.error("some error occured in adding to cart");
+                      console.error(error);
+                      setShowToast(true);
+                      setToastProperties({
+                        toastType: "error",
+                        toastMessage: "some error occured in adding to cart",
                       });
-                  }}
-                >
-                  Add to cart
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-success me-2"
-                  onClick={(event) => {
-                    event.stopPropagation();
-                  }}
-                >
-                  Buy Now
-                </button>
-              </div>
+                    });
+                }}
+              >
+                Add to cart
+              </button>
+              <button
+                type="button"
+                className="btn btn-success me-2"
+                onClick={(event) => {
+                  event.stopPropagation();
+                }}
+              >
+                Buy Now
+              </button>
             </div>
           </div>
-        </Link>
+        </div>
       </div>
     </>
   );
