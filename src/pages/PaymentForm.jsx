7aa82c35a6027cf551e16af429d0aefe3d16af55
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2023-07-15 14:29:31 +0530

    payment

--- src/pages/PaymentForm.jsx
+++ src/pages/PaymentForm.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import Logo from "../assets/logo.jpeg";
 
+const validationSchema = Yup.object().shape({
+  nameOnCard: Yup.string().when("paymentMethod", {
+    is: (value) => value === "credit" || value === "debit",
+    then: Yup.string().required("Name on card is required"),
+  }),
+  cardNumber: Yup.string().when("paymentMethod", {
+    is: (value) => value === "credit" || value === "debit",
+    then: Yup.string()
+      .required("Card number is required")
+      .matches(/^[0-9]{16}$/, "Card number must be 16 digits"),
+  }),
+  expirationDate: Yup.string().when("paymentMethod", {
+    is: (value) => value === "credit" || value === "debit",
+    then: Yup.string()
+      .required("Expiration date is required")
+      .matches(
+        /^(0[1-9]|1[0-2])\/([0-9]{2})$/,
+        "Expiration date must be in the format MM/YY"
+      ),
+  }),
+  cvv: Yup.string().when("paymentMethod", {
+    is: (value) => value === "credit" || value === "debit",
+    then: Yup.string()
+      .required("CVV is required")
+      .matches(/^[0-9]{3}$/, "CVV must be 3 digits"),
+  }),
+});
+
 function PaymentForm() {
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+  const { errors } = formState;
+  const paymentMethod = watch("paymentMethod");
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="container">
@@ -15,16 +56,16 @@ function PaymentForm() {
           <h5>KalaKriti</h5>
         </div>
         <h4 className="mb-3">Payment</h4>
-        <form className="needs-validation" noValidate="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="my-3">
             <div className="form-check">
               <input
+                {...register("paymentMethod")}
                 id="POD"
-                name="paymentMethod"
                 type="radio"
                 className="form-check-input"
+                value="POD"
                 defaultChecked=""
-                required=""
               />
               <label className="form-check-label" htmlFor="POD">
                 Pay On Delivery
@@ -32,11 +73,11 @@ function PaymentForm() {
             </div>
             <div className="form-check">
               <input
+                {...register("paymentMethod")}
                 id="credit"
-                name="paymentMethod"
                 type="radio"
                 className="form-check-input"
-                required=""
+                value="credit"
               />
               <label className="form-check-label" htmlFor="credit">
                 Credit card
@@ -44,11 +85,11 @@ function PaymentForm() {
             </div>
             <div className="form-check">
               <input
+                {...register("paymentMethod")}
                 id="debit"
-                name="paymentMethod"
                 type="radio"
                 className="form-check-input"
-                required=""
+                value="debit"
               />
               <label className="form-check-label" htmlFor="debit">
                 Debit card
@@ -56,82 +97,94 @@ function PaymentForm() {
             </div>
             <div className="form-check">
               <input
+                {...register("paymentMethod")}
                 id="paypal"
-                name="paymentMethod"
                 type="radio"
                 className="form-check-input"
-                required=""
+                value="paypal"
               />
               <label className="form-check-label" htmlFor="paypal">
                 PayPal
               </label>
             </div>
           </div>
-          <div className="row gy-3">
-            <div className="col-md-6">
-              <label htmlFor="cc-name" className="form-label">
-                Name on card
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="cc-name"
-                placeholder=""
-                required=""
-              />
-              <small className="text-body-secondary">
-                Full name as displayed on card
-              </small>
-              <div className="invalid-feedback">Name on card is required</div>
-            </div>
-            <div className="col-md-6">
-              <label htmlFor="cc-number" className="form-label">
-                Card number
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="cc-number"
-                placeholder=""
-                required=""
-              />
-              <div className="invalid-feedback">
-                Credit card number is required
+
+          {(paymentMethod === "credit" || paymentMethod === "debit") && (
+            <>
+              <div className="row gy-3">
+                <div className="col-md-4">
+                  <label htmlFor="cc-name" className="form-label">
+                    Name on card
+                  </label>
+                  <input
+                    {...register("nameOnCard")}
+                    type="text"
+                    className={`form-control ${
+                      errors.nameOnCard ? "is-invalid" : ""
+                    }`}
+                    id="cc-name"
+                    placeholder=""
+                  />
+                  <small className="text-body-secondary">
+                    Full name as displayed on card
+                  </small>
+                  <div className="invalid-feedback">
+                    {errors.nameOnCard?.message}
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <label htmlFor="cc-number" className="form-label">
+                    Card number
+                  </label>
+                  <input
+                    {...register("cardNumber")}
+                    type="text"
+                    className={`form-control ${
+                      errors.cardNumber ? "is-invalid" : ""
+                    }`}
+                    id="cc-number"
+                    placeholder=""
+                  />
+                  <div className="invalid-feedback">
+                    {errors.cardNumber?.message}
+                  </div>
+                </div>
+                <div className="col-md-2">
+                  <label htmlFor="cc-expiration" className="form-label">
+                    Expiration
+                  </label>
+                  <input
+                    {...register("expirationDate")}
+                    type="text"
+                    className={`form-control ${
+                      errors.expirationDate ? "is-invalid" : ""
+                    }`}
+                    id="cc-expiration"
+                    placeholder=""
+                  />
+                  <div className="invalid-feedback">
+                    {errors.expirationDate?.message}
+                  </div>
+                </div>
+                <div className="col-md-2">
+                  <label htmlFor="cc-cvv" className="form-label">
+                    CVV
+                  </label>
+                  <input
+                    {...register("cvv")}
+                    type="password"
+                    className={`form-control ${errors.cvv ? "is-invalid" : ""}`}
+                    id="cc-cvv"
+                    placeholder=""
+                  />
+                  <div className="invalid-feedback">{errors.cvv?.message}</div>
+                </div>
               </div>
-            </div>
-            <div className="col-md-3">
-              <label htmlFor="cc-expiration" className="form-label">
-                Expiration
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="cc-expiration"
-                placeholder=""
-                required=""
-              />
-              <div className="invalid-feedback">Expiration date required</div>
-            </div>
-            <div className="col-md-3">
-              <label htmlFor="cc-cvv" className="form-label">
-                CVV
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="cc-cvv"
-                placeholder=""
-                required=""
-              />
-              <div className="invalid-feedback">Security code required</div>
-            </div>
-          </div>
+            </>
+          )}
+
           <hr className="my-4" />
-          <button
-            className="w-100 btn btn-primary btn-lg"
-            type="submit"
-            // disabled={!addressSelectionDone}
-          >
+          <button className="w-100 btn btn-primary btn-lg" type="submit">
             Pay
           </button>
         </form>
